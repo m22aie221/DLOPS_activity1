84a4ca0ab389d1f6009681e7d8f4491abbddb02e
Rajesh Mohan <m22aie221@iitj.ac.in>
2024-04-04 17:19:22 +0530

    Activity Q2.c

--- Q2.py
+++ Q2.py
@@ -11,6 +11,9 @@ import matplotlib.pyplot as plt
 
 # Generate sample points
 x_random_values = np.array([-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6])
+
+x_random_values = np.sort(x_random_values)
+
 x = x_random_values
 
 # Get out for sigmoid funtion
